7c4f99cf9679c461437d2028c2d6c5c73153c16b
Ilya Lukanchev <bkmz432111@yandex.ru>
2025-06-10 22:00:08 +0400

    1

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,35 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context = 
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+
+      userService.addCar(new Car("BMW", 3));
+      userService.add(new User("Ilya", "Lukanchev", "bkmz432111@yandex.ru"));
+
+
+
+      List<User> users = userService.listUsers();
+      for (User user : users) {
+         System.out.println("Id = "+user.getId());
+         System.out.println("First Name = "+user.getFirstName());
+         System.out.println("Last Name = "+user.getLastName());
+         System.out.println("Email = "+user.getEmail());
+         System.out.println();
+      }
+
+      context.close();
+   }
+}
